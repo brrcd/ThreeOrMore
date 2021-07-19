cca9ebbe3daacdbb71349b10ca2aa2e4b384c6dd
mawxim <brrcd@ya.ru>
2021-07-19 22:06:17 +0300

    dev branch init commit

--- config.lua
+++ config.lua
@@ -23,6 +23,10 @@ function Config:GetThemeColor()
 	return c.r, c.g, c.b, c.hex;
 end
 
+local FIELDSIZE = 5;
+local ROWSCOUNT = FIELDSIZE;
+local COLUMNSCOUNT = FIELDSIZE;
+
 function Config:CreateButton(point, relativeFrame, relativePoint, xOffset, yOffset, text)
 	local btn = CreateFrame("Button", nil, UIConfig, "GameMenuButtonTemplate");
 	btn:SetPoint(point, relativeFrame, relativePoint, xOffset, yOffset);
@@ -35,51 +39,59 @@ end
 
 local isCursorChanged = false;
 
-
-
 function Config:CreateMenu()
 	--main frame
 	UIConfig = CreateFrame("Frame", "ThreeOrMoreMasterFrame", UIParent, "ButtonFrameTemplate");
 	UIConfig:SetSize(400, 400);
 	UIConfig:SetPoint("CENTER");
 
+	--game field frame
 	local GameWindow = CreateFrame("Frame", "GameWindowFrame", UIConfig, "ThinBorderTemplate");
 	GameWindow:SetSize(388, 312);
 	GameWindow:SetPoint("CENTER", -1, -18);
 
-	CreateActionBarSlot = function(xOffset)
-		local slot = CreateFrame("Frame", nil, GameWindow, "MultiBarButtonTemplate");
-		local texture = slot:CreateTexture();
+	CreateCell = function(xOffset, yOffset)
+		local cell = CreateFrame("Button", nil, GameWindow, "MultiBarButtonTemplate");
+		local texture = cell:CreateTexture();
 		local cursor;
 	
-		slot:SetPoint("CENTER", GameWindow, "TOPLEFT", xOffset, -23);
-		slot:EnableMouse(true);
-		slot:RegisterForDrag("LeftButton", "RightButton");
+		cell:SetPoint("CENTER", GameWindow, "TOPLEFT", xOffset, yOffset);
+		cell:EnableMouse(true);
+		cell:RegisterForDrag("LeftButton", "RightButton");
 		
 		texture:SetAllPoints();
 		texture:SetTexture("interface/icons/inv_mushroom_11")
+
+		cell:SetScript("OnClick", function()
+			ActionButton_ShowOverlayGlow(self)
+		end)
 	
-		return slot;
+		return cell;
 	end
 	
-	CreateActionBar = function(slotsQuant)
-		local slotWidth = 40;
-		local slotOffset = 23;
+	CreateGameField = function(columnsCount, rowsCount)
+		local cellWidth = 40;
+		local xOffset = 23;
+		local yOffset = -23;
 	
-		for x = slotsQuant,1,-1 do
-			UIConfig.slot = CreateActionBarSlot(slotOffset);
-			slotOffset = slotOffset + slotWidth;
+		for y = rowsCount, 1, -1 do
+			for x = columnsCount, 1, -1 do
+				UIConfig.cell = CreateCell(xOffset, yOffset);
+				xOffset = xOffset + cellWidth;
+			end
+			yOffset = yOffset - cellWidth;
+			xOffset = 23;
 		end
 	end
 
-	UIConfig.slotBar = CreateActionBar(5);
+	UIConfig.gameField = CreateGameField(ROWSCOUNT, COLUMNSCOUNT);
 
 	--making main frame movable
-	-- UIConfig:SetMovable(true);
-	-- UIConfig:EnableMouse(true);
-	-- UIConfig:RegisterForDrag("LeftButton");
-	-- UIConfig:SetScript("OnDragStart", UIConfig.StartMoving);
-	-- UIConfig:SetScript("OnDragStop", UIConfig.StopMovingOrSizing);
+	UIConfig:SetMovable(true);
+	UIConfig:EnableMouse(true);
+	UIConfig:RegisterForDrag("LeftButton");
+	UIConfig:SetScript("OnDragStart", UIConfig.StartMoving);
+	UIConfig:SetScript("OnDragStop", UIConfig.StopMovingOrSizing);
 
 	--portrait
 	UIConfig.portrait:SetTexture("Interface\\GLUES\\CHARACTERCREATE\\UI-CharacterCreate-Factions");
