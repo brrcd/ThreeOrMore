18b7a4c4a34a3ef6a0f97d1c093ce41f776c304f
mawxim <brrcd@ya.ru>
2021-08-08 21:44:36 +0300

    changing matched cells after swap

--- config.lua
+++ config.lua
@@ -26,8 +26,8 @@ end
 ------------------------------------------------------------------------
 
 local FIELD_SIZE = 5;
-local ROWS_COUNT = FIELD_SIZE+1;
-local COLUMNS_COUNT = FIELD_SIZE+3;
+local ROW_X = FIELD_SIZE+1;
+local COL_X = FIELD_SIZE+3;
 
 Race = {
     getRace = function (index)
@@ -95,39 +95,50 @@ function Config:CreateMenu()
 		return{
 			hideGlow = function ()
 				GlowButtonEffect:Hide();
+			end,
+			showGlow = function ()
+				GlowButtonEffect:Show();
 			end
 		}
 	end
 
-	FieldCells = {};
-	CellsArray = {};
+	FieldCells = {}; --CreateCell function Array
+	CellsArrayForGlow = {}; --Cell array
+	local master_cell;
+	local direction; --horizontal 0 or vertical 1 swap
+	local swap_done = false;
 
 	local swap_two_cells = function (fc, sc, fr, sr)
 		print("Начало смены клеток.");
 		FieldCells[fc].setRace(sr);
 		FieldCells[sc].setRace(fr);
+		master_cell = sc;
+		swap_done = true;
 		print("Конец смены клеток.");
 	end
 
-	--column checkin
+	--column checking for match three to allow swap
+		--if our cell is first from top check
 	local chk_col_top = function (f_index, s_index)
-		return FieldCells[s_index+COLUMNS_COUNT+1].getRace() ==
-		FieldCells[s_index+2*(COLUMNS_COUNT+1)].getRace() and
-		FieldCells[s_index+2*(COLUMNS_COUNT+1)].getRace() ==
+		return FieldCells[s_index+COL_X+1].getRace() ==
+		FieldCells[s_index+2*(COL_X+1)].getRace() and
+		FieldCells[s_index+2*(COL_X+1)].getRace() ==
 		FieldCells[f_index].getRace();
 	end
 
+		--if our cell is at middle
 	local chk_col_mid = function (f_index, s_index)
-		return FieldCells[s_index-COLUMNS_COUNT-1].getRace() ==
-		FieldCells[s_index+COLUMNS_COUNT+1].getRace() and
-		FieldCells[s_index+COLUMNS_COUNT+1].getRace() ==
+		return FieldCells[s_index-COL_X-1].getRace() ==
+		FieldCells[s_index+COL_X+1].getRace() and
+		FieldCells[s_index+COL_X+1].getRace() ==
 		FieldCells[f_index].getRace();
 	end
 
+		--if our cell is last from top check
 	local chk_col_bot = function (f_index, s_index)
-		return FieldCells[s_index-COLUMNS_COUNT-1].getRace() ==
-		FieldCells[s_index-2*(COLUMNS_COUNT+1)].getRace() and
-		FieldCells[s_index-2*(COLUMNS_COUNT+1)].getRace() ==
+		return FieldCells[s_index-COL_X-1].getRace() ==
+		FieldCells[s_index-2*(COL_X+1)].getRace() and
+		FieldCells[s_index-2*(COL_X+1)].getRace() ==
 		FieldCells[f_index].getRace();
 	end
 
@@ -155,90 +166,98 @@ function Config:CreateMenu()
 
 	local chk_horizontal = function (f_c, s_c, f_r, s_r)
 		--if we swap at first row to prevent crash
-		if (s_c <= COLUMNS_COUNT) then
+		if (s_c <= COL_X) then
 			if(chk_col_top(f_c, s_c)) then
 				swap_two_cells(f_c,s_c,f_r,s_r);
+				direction = 0;
 			end
 		--same for last
-		elseif (s_c >= ((COLUMNS_COUNT+1) * ROWS_COUNT)) then
+		elseif (s_c >= ((COL_X+1) * ROW_X)) then
 			if(chk_col_bot(f_c, s_c)) then
 				swap_two_cells(f_c,s_c,f_r,s_r);
+				direction = 0;
 			end
 		--second from top
-		elseif (s_c > COLUMNS_COUNT and s_c < ((COLUMNS_COUNT+1)*2)) then
+		elseif (s_c > COL_X and s_c < ((COL_X+1)*2)) then
 			if(chk_col_top(f_c, s_c) or chk_col_mid(f_c, s_c)) then
 				swap_two_cells(f_c,s_c,f_r,s_r);
+				direction = 0;
 			end
 		--second from bot
-		elseif (s_c >= ((COLUMNS_COUNT+1) * (ROWS_COUNT-1)) and
-			s_c < ((COLUMNS_COUNT+1) * ROWS_COUNT)) then
+		elseif (s_c >= ((COL_X+1) * (ROW_X-1)) and
+			s_c < ((COL_X+1) * ROW_X)) then
 			if(chk_col_mid(f_c, s_c) or chk_col_bot(f_c, s_c)) then
 				swap_two_cells(f_c,s_c,f_r,s_r);
+				direction = 0;
 			end
 		elseif (chk_col_top(f_c, s_c) or
 			chk_col_mid(f_c, s_c) or
 			chk_col_bot(f_c, s_c)) then
 				swap_two_cells(f_c,s_c,f_r,s_r);
-		else
-			print("horizontal")
+				direction = 0;
 		end
 	end
 
 	local chk_vertical = function (f_c, s_c, f_r, s_r)
-		--if we swap at first column to prevent crash
-		if (math.fmod((s_c+(COLUMNS_COUNT+1)), (COLUMNS_COUNT+1)) == 0) then
+		--first column
+		if (math.fmod((s_c+(COL_X+1)), (COL_X+1)) == 0) then
 			if(chk_row_left(f_c, s_c)) then
 				swap_two_cells(f_c,s_c,f_r,s_r);
+				direction = 1;
 			end
-		elseif (math.fmod((s_c+(COLUMNS_COUNT+1)), (COLUMNS_COUNT+1)) == 1) then
+		--second
+		elseif (math.fmod((s_c+(COL_X+1)), (COL_X+1)) == 1) then
 			if(chk_row_left(f_c, s_c) or chk_row_mid(f_c, s_c)) then
 				swap_two_cells(f_c,s_c,f_r,s_r);
+				direction = 1;
 			end
-		elseif (math.fmod((s_c+(COLUMNS_COUNT+1)), (COLUMNS_COUNT+1)) == (COLUMNS_COUNT-1)) then
+		--penult
+		elseif (math.fmod((s_c+(COL_X+1)), (COL_X+1)) == (COL_X-1)) then
 			if(chk_row_mid(f_c, s_c) or chk_row_right(f_c, s_c)) then
 				swap_two_cells(f_c,s_c,f_r,s_r);
+				direction = 1;
 			end
-		elseif (math.fmod((s_c+(COLUMNS_COUNT+1)), (COLUMNS_COUNT+1)) == COLUMNS_COUNT) then
+		--last
+		elseif (math.fmod((s_c+(COL_X+1)), (COL_X+1)) == COL_X) then
 			if(chk_row_right(f_c, s_c)) then
 				swap_two_cells(f_c,s_c,f_r,s_r);
+				direction = 1;
 			end
 		elseif (chk_row_left(f_c, s_c) or
 			chk_row_mid(f_c, s_c) or
 			chk_row_right(f_c, s_c)) then
 				swap_two_cells(f_c,s_c,f_r,s_r);
-		else
-			print("vertical")
+				direction = 1;
 		end
 	end
 
-	local f_R;
-	local s_R;
-	local f_C;
-	local s_C;
+	local f_R; --first cell race
+	local s_R; --second cell race
+	local f_C; --first cell
+	local s_C; --second cell
 	local glow_button
 
 	CreateCell = function(xOffset, yOffset, n)
 		local race;
 		local index = n;
-		local index_RACE;
+		local index_RACE; --we are getting it on Cell creation
 		Cell = CreateFrame("Button", nil, UIConfig, "MultiBarButtonTemplate");
 		local texture = Cell:CreateTexture();
-		local cell = Cell;
-		CellsArray[index] = cell;
+		-- local cell = Cell;
+		CellsArrayForGlow[index] = Cell;
 	
 		Cell:SetPoint("CENTER", GameWindow, "TOPLEFT", xOffset, yOffset);
 		Cell:EnableMouse(true);
 		Cell:RegisterForDrag("LeftButton", "RightButton");
 
 		Cell:SetScript("OnClick", function()
-			ActionButton_ShowOverlayGlow(self);
 
 			--this one swaps cells
 			if (f_R == nil) then
 				f_R = index_RACE;
 				f_C = index;
 				--creating glow effect on first click
-				glow_button = GlowButton(CellsArray[index]);
+				glow_button = GlowButton(CellsArrayForGlow[index]);
 				print("Первая клетка расы :", race, ". Под индексом :", index);
 			elseif (f_R ~= nil and s_R == nil) then
 				s_R = index_RACE;
@@ -248,13 +267,28 @@ function Config:CreateMenu()
 				print("Вторая клетка расы :", race, ". Под индексом :", index);
 
 				--checking if we clicking neighbour cells
-				if (math.abs(f_C - s_C) == 1 or math.abs(f_C - s_C) == (COLUMNS_COUNT + 1)) then
+				if (math.abs(f_C - s_C) == 1 or math.abs(f_C - s_C) == (COL_X + 1)) then
 					chk_horizontal(f_C, s_C, f_R, s_R);
 					chk_vertical(f_C, s_C, f_R, s_R);
+					chk_horizontal(s_C, f_C, s_R, f_R);
+					chk_vertical(s_C, f_C, s_R, f_R);
+					--TODO: fix nil exceptions
+					if(swap_done) then
+						if (direction == 0) then
+							FieldCells[master_cell].setRandomRace();
+							FieldCells[master_cell+(COL_X+1)].setRandomRace();
+							FieldCells[master_cell+(2*(COL_X+1))].setRandomRace();
+						elseif (direction == 1) then
+							FieldCells[master_cell].setRandomRace();
+							FieldCells[master_cell+1].setRandomRace();
+							FieldCells[master_cell+2].setRandomRace();
+						end
+						swap_done = false;
+					end
 				else
-					print("select other cell")
+					print("Неподходящая клетка!")
 				end;
-
+				
 				f_R = nil;
 				s_R = nil;
 			end
@@ -298,32 +332,41 @@ function Config:CreateMenu()
 	end
 
 	FillFieldWithRaces = function()
-        local rowsDoublesCount = 0;
-        local prevRowCell;
+        local row_dbl_COUNT = 0;
+        local prev_row_CELL;
 
         --initialize table for column doubles
-		local prevColumnCell = {};
-        local cX = COLUMNS_COUNT;
-        local columnDoublesCount = {};
+		local prev_col_CELL = {};
+        local cX = COL_X;
+        local col_dbl_COUNT = {};
         for x = 0, cX, 1 do
-            columnDoublesCount[x] = 0;
+            col_dbl_COUNT[x] = 0;
         end
 
 		for x = 0, #FieldCells, 1 do
 			FieldCells[x].setRandomRace();
-			if (x > 1 and prevRowCell == FieldCells[x].getRace()) then
-				rowsDoublesCount = rowsDoublesCount + 1;
+			--row double checking to prevent cases with 3 in a row on field generation
+			if (x > 1 and prev_row_CELL == FieldCells[x].getRace()) then
+				row_dbl_COUNT = row_dbl_COUNT + 1;
 			end;
-			while (rowsDoublesCount == 1) do
+			while (row_dbl_COUNT == 1) do
 				FieldCells[x].setRandomRace();
-				rowsDoublesCount = 0;
+				row_dbl_COUNT = 0;
 			end;
-			prevRowCell = FieldCells[x].getRace();
-			-- TODO: column doubles checking
+			--column double checking
+			if (x > cX and prev_col_CELL[x-(cX+1)] == FieldCells[x].getRace()) then
+				col_dbl_COUNT[math.fmod(x, (cX+1))] = col_dbl_COUNT[math.fmod(x, (cX+1))] + 1;
+			end
+			while (col_dbl_COUNT[math.fmod(x, (cX+1))] == 1) do
+				FieldCells[x].setRandomRace();
+				col_dbl_COUNT[math.fmod(x, (cX+1))] = 0;
+			end
+			prev_row_CELL = FieldCells[x].getRace();
+			prev_col_CELL[x] = FieldCells[x].getRace();
 		end;
 	end
 
-	UIConfig.emptyField = CreateEmptyField(ROWS_COUNT, COLUMNS_COUNT);
+	UIConfig.emptyField = CreateEmptyField(ROW_X, COL_X);
 
 	--making main frame movable
 	UIConfig:SetMovable(true);
@@ -347,12 +390,16 @@ function Config:CreateMenu()
 		FillFieldWithRaces();
 		f_R = nil;
 		s_R = nil;
+		print(#GlowButtonsArray, #FieldCells)
 	end);
+
 	UIConfig.resetButton = self:CreateButton("CENTER", UIConfig, "BOTTOM", 0, 15, "Reset");
+
     UIConfig.helloButton = self:CreateButton("CENTER", UIConfig, "BOTTOM", 125, 15, "Hello");
 	UIConfig.helloButton:SetScript("OnClick", function ()
 		print("Зачем ты нажал сюда?");
 	end)
 	UIConfig:Hide();
+	
 	return UIConfig;
 end
